cd27fc21f35bcc7f5d5d0322746c5e2d3a168f3b
1Maxuta1 <maksklas_1@ukr.net>
2023-04-29 23:28:04 +0300

    Header + AboutUs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,55 @@
-import React from 'react';
-import Navigation from './Navigation';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
+import Dropdown from "./buttons/Dropdown";
+import { Link } from "react-scroll";
+import HeaderDropdownMobile from "./buttons/HeaderDropdownMobile";
+import NavLink from "./NavLink/NavLink";
 
 const Header = () => {
   return (
-    <header className='relative z-50' id='header'>
-      <Navigation />
-      <div className=''>
-        <div className='overflow-clip lg:h-[35rem] h-[65vh] relative -z-10'>
-          <div className='absolute lg:bottom-36 lg:left-8 bottom-1/2 translate-y-1/2 lg:translate-0 text-brightViolet lg:w-[800px] w-full lg:text-5xl bg-black bg-opacity-60 rounded-md max-w-[90%] left-1/2 -translate-x-1/2 text-2xl lg:-translate-x-0 py-5 px-3'>
-            <h1 className='text-center py-8 px-3 lg:p-0 lg:text-left font-bold border-red-500'>
-              With our wide range of experience, we renovate & build everything
-              from residential to commercial projects
-            </h1>
-          </div>
+    <nav className="navigation sticky top-0">
+      <div className="navigation__wrapper  flex justify-between sm:gap-8 lg:gap-10 flex-wrap items-center  flex-row lg:h-20 px-10 bg-bg">
+        <div className="navigation__logo w-40  lg:mr-48 flex items-center">
+          <a className="navigation__logo link">
+            <picture>
+              <source media="(max-width: 1023px)" srcSet="/img/logo-sm.webp" />
+              <source media="(min-width: 1024px)" srcSet="/img/logo.webp" />
+              <img
+                src="/img/logo-v2.png"
+                className="flex items-center w-20 h-20"
+                alt="Remsvol"
+              />
+            </picture>
+          </a>
         </div>
+        <div className="navigation__items hidden md:hidden lg:block">
+          <ul className="flex flex-row gap-6 md:gap-4 lg:gap-6 font-bold lg:text-lg">
+            <NavLink href="about-us" text="ABOUT US" />
+            <li className="navigation__item duration-300 hover:translate-y-[-0.2rem] text-white">
+              <Dropdown />
+            </li>
+            <NavLink href="reviews" text="REVIEWS" />
+            <NavLink href="contact-us" text="CONTACT US" />
+          </ul>
+        </div>
+
+        {/* SKRYTO na mobilah  */}
+        <a
+          href="tel:(281) 829-9828"
+          className="navigation__button hidden lg:block text-whiteColor cursor-pointer p-3 rounded-3xl duration-300 hover:text-ultraViolet text-lg font-bold "
+        >
+          <p>(281) 829-9828</p>
+        </a>
+        <a
+          href="tel: (832) 816-8860"
+          className="navigation__button hidden lg:block bg-ultraViolet hover:bg-brightViolet text-whiteColor cursor-pointer p-3 rounded-3xl duration-300 hover:-translate-y-1 transition-all text-lg font-bold lg:px-5"
+        >
+          CALL NOW
+        </a>
+
+        <HeaderDropdownMobile />
       </div>
-    </header>
+    </nav>
   );
 };
 
--- src/components/buttons/HeaderDropdownMobile.jsx
+++ src/components/buttons/HeaderDropdownMobile.jsx
@@ -1,112 +1,112 @@
-import { useState } from 'react'
-import React from 'react'
-import { Link } from 'react-scroll'
-import { useSpring, animated } from 'react-spring'
+import { useState } from "react";
+import React from "react";
+import { Link } from "react-scroll";
+import { useSpring, animated } from "react-spring";
 
 function HeaderDropdownMobile() {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
   const props = useSpring({
-    pointerEvents: isOpen ? 'all' : 'none',
+    pointerEvents: isOpen ? "all" : "none",
     opacity: isOpen ? 1 : 0,
     right: isOpen ? 0 : -700,
     top: isOpen ? 0 : 0,
-  })
+  });
 
   const toggleDropdown = () => {
-    setIsOpen(!isOpen)
-  }
+    setIsOpen(!isOpen);
+  };
   const handleLinkClick = (e) => {
-    e.preventDefault()
-    toggleDropdown()
-    console.log(e.target.innerText)
-  }
+    e.preventDefault();
+    toggleDropdown();
+    console.log(e.target.innerText);
+  };
   return (
     <>
-      <div className='relative lg:hidden'>
+      <div className="relative lg:hidden">
         <button
-          className='rounded flex justify-center items-center'
+          className="rounded flex justify-center items-center"
           onClick={toggleDropdown}
         >
           <svg
-            stroke='currentColor'
-            fill='#392820'
-            strokeWidth='0'
-            viewBox='0 0 512 512'
-            height='2em'
-            width='2em'
-            xmlns='http://www.w3.org/2000/svg'
+            stroke="currentColor"
+            className="fill-whiteColor"
+            strokeWidth="0"
+            viewBox="0 0 512 512"
+            height="2em"
+            width="2em"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            <path d='M32 96v64h448V96H32zm0 128v64h448v-64H32zm0 128v64h448v-64H32z'></path>
+            <path d="M32 96v64h448V96H32zm0 128v64h448v-64H32zm0 128v64h448v-64H32z"></path>
           </svg>
         </button>
       </div>
       <animated.div
-        className='fixed z-50 bg-[#c09559] shadow-lg rounded h-screen w-screen'
+        className="fixed z-50 bg-bg shadow-lg rounded h-screen w-screen"
         style={props}
       >
-       <div className='h-full relative'>
-          <button onClick={toggleDropdown} className='absolute right-10 top-5'>
+        <div className="h-full relative">
+          <button onClick={toggleDropdown} className="absolute right-10 top-5">
             <svg
-              stroke='currentColor'
-              fill='#392820'
-              strokeWidth='0'
-              viewBox='0 0 1024 1024'
-              height='2em'
-              width='2em'
-              xmlns='http://www.w3.org/2000/svg'
+              className="fill-whiteColor"
+              strokeWidth="0"
+              viewBox="0 0 1024 1024"
+              height="2em"
+              width="2em"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <path d='M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 0 0 203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z'></path>
+              <path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 0 0 203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z"></path>
             </svg>
           </button>
-          <ul className=' py-12 px-10 text-left w-[380px]  text-white  '>
-            <Link to='about-us' smooth={true} duration={500}>
+          <ul className=" py-12 px-10 text-left w-[380px]  text-white  ">
+            <Link to="about-us" smooth={true} duration={500}>
               <li
-                href='#'
-                className='block px-4 py-2 h-full  hover:bg-[#392820]  duration-300 '
+                href="#"
+                className=" text-whiteColor block px-4 py-2 h-full  hover:bg-ultraViolet  duration-300 "
                 onClick={handleLinkClick}
               >
                 About us
               </li>
             </Link>
-            <Link to='services' smooth={true} duration={500}>
+            <Link to="services" smooth={true} duration={500}>
               <li
-                href='#'
-                className='block px-4 py-2 hover:bg-[#392820] duration-300 '
+                href="#"
+                className=" text-whiteColor block px-4 py-2 hover:bg-ultraViolet duration-300 "
                 onClick={handleLinkClick}
               >
                 Services
               </li>
             </Link>
-            <Link to='reviews' smooth={true} duration={500}>
+            <Link to="reviews" smooth={true} duration={500}>
               <li
-                href='#'
-                className='block px-4 py-2 hover:bg-[#392820] duration-300 '
+                href="#"
+                className=" text-whiteColor block px-4 py-2 hover:bg-ultraViolet duration-300 "
                 onClick={handleLinkClick}
               >
                 Reviews
               </li>
             </Link>
-            <Link to='contact-us' smooth={true} duration={500}>
+            <Link to="contact-us" smooth={true} duration={500}>
               <li
-                href='#'
-                className='block px-4 py-2 hover:bg-[#392820] duration-300 '
+                href="#"
+                className=" text-whiteColor block px-4 py-2 hover:bg-ultraViolet duration-300 "
                 onClick={handleLinkClick}
               >
                 Contact us
               </li>
             </Link>
-            <Link to='apply-form' onClick={handleLinkClick} smooth={true} duration={500}>
-            <div className='mx-4 py-2 w-fit navigation__button cursor-pointer p-1 duration-300 border-b'>
-                Apply now
-            </div>
-          </Link>
+
+            <a
+              href="tel: (832) 816-8860"
+              className="text-whiteColor mx-4 py-2 w-fit navigation__button cursor-pointer p-1 duration-300 border-b"
+            >
+              Call now
+            </a>
           </ul>
-          
-       </div>
+        </div>
       </animated.div>
     </>
-  )
+  );
 }
 
-export default HeaderDropdownMobile
+export default HeaderDropdownMobile;
