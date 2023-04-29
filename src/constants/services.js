5fd84910677f20075df64499a1b9ec901d4497cf
Tymofii Antoniuk <111820939+timaantonuk@users.noreply.github.com>
2023-04-29 14:55:45 -0500

    services section

--- src/constants/services.js
+++ src/constants/services.js
@@ -1,44 +1,47 @@
 export const tiers = [
-    {
-      name: 'Residential',
-      id: 'tier-residential',
-      href: '#',
-      description: 'The essentials to provide your best work for clients.',
-      features: [
-        'Our flexible budget optimization methodology allows for the best project management and completion',
-      ],
-      featured: false,
-      img: '/img/residential.png',
-      smallImg: '/img/residential-sm.jpg',
-      cta: 'Apply',
-      color: 'bg-[#986235]',
-    },
-    {
-      name: 'Commercial',
-      id: 'tier-commercial',
-      href: '#',
-      description: 'A plan that scales with your rapidly growing business.',
-      features: [
-        'Building on the 25+ years of experience & excellence. At Remsvol, customers rely on our stature in the industry',
-      ],
-      featured: false,
-      img: '/img/commercial.png',
-      smallImg: '/img/commercial-sm.jpg',
-      cta: 'Apply',
-      color: 'bg-[#986235]',
-    },
-    {
-      name: 'Custom',
-      id: 'tier-custom',
-      href: '#',
-      description: 'Dedicated support and infrastructure for your company.',
-      features: [
-        'With an extensive selection of materials, professionals & methodologies, at Remsvol your ideas will be executed at the highest standards.',
-      ],
-      featured: true,
-      img: '/img/custom.png',
-      smallImg: '/img/custom-sm.jpg',
-      cta: 'Apply',
-      color: 'bg-[#392820]',
-    },
-  ];
+  {
+    name: 'Sewer & Drain Cleaning Works',
+    id: 'sewer',
+    href: '#',
+    description:
+      'Our top of the line drain equipment allows our plumbers to use time-eﬃcient and low-cost techniques to help clear your clogged drains and sewer lines',
+    features: [
+      'Our top of the line drain equipment allows our plumbers to use time-eﬃcient and low-cost techniques to help clear your clogged drains and sewer lines',
+    ],
+    featured: false,
+    img: '/img/services-icons/sewer-repair.png',
+    smallImg: '/img/services-icons/sewer-repair.png',
+    cta: 'Apply',
+    color: '',
+  },
+  {
+    name: 'Plumbing Repair & Replacement',
+    id: 'plumbing-repair',
+    href: '#',
+    description:
+      'Our team of experienced plumbing technicians are ready to handle any plumbing issue you may be experiencing from diagnosis to repair. Just contact us.',
+    features: [
+      'Building on the 25+ years of experience & excellence. At Remsvol, customers rely on our stature in the industry',
+    ],
+    featured: false,
+    img: '/img/services-icons/plumbing-fix.png',
+    smallImg: '/img/services-icons/plumbing-fix.png',
+    cta: 'Apply',
+    color: '',
+  },
+  {
+    name: 'Water Heaters Fixtures & Toilets',
+    id: 'heaters-toilets',
+    href: '#',
+    description:
+      'Whether you need to replace your old model with a high efficiency water heater, a new toilet or fixture, our experienced plumbers are ready for the job.',
+    features: [
+      'With an extensive selection of materials, professionals & methodologies, at Remsvol your ideas will be executed at the highest standards.',
+    ],
+    featured: true,
+    img: '/img/services-icons/toilet-fix.png',
+    smallImg: '/img/services-icons/toilet-fix.png',
+    cta: 'Apply',
+    color: '',
+  },
+];
