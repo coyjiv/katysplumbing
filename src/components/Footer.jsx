b0aa9efe6cbd76d4c4d773486b9e7fc46342e92d
Daniil Shcherbakov <dshcherbakov@seamgroup.com>
2023-05-06 20:34:04 +0200

    small portions of fixes#2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,43 +21,42 @@ const navigation = {
 
 export default function Footer() {
   return (
-    <footer className="bg-ultraViolet h-56">
-      <div className="mx-auto max-w-7xl overflow-hidden px-6 pt-[60px] lg:px-8">
-        <nav
-          className="-mb-6 text-whiteColor/60 columns-4 lg:columns-2 sm:flex sm:justify-center gap-0 lg:gap-4 text-center"
-          aria-label="Footer"
-        >
-          {navigation.main.map((item, i) => (
-            <Link
-              to={item.scroll}
-              href={"#"+item.scroll}
-              smooth={true}
-              duration={500}
-              offset={navigation.main.length - 1 === i ? -100 : -50}
-              key={i}
-            >
-              <div className="pb-6">
-                <p className="text-md cursor-pointer font-display leading-6 hover:text-whiteColor duration-300">
+    <footer className="bg-ultraViolet">
+      <div className="mx-auto max-w-7xl overflow-hidden px-6 pt-[60px] lg:pb-9 lg:px-8">
+        <div className="flex flex-col gap-6">
+          <nav
+            className="-mb-6 text-whiteColor/60 columns-2 lg:columns-2 sm:flex sm:justify-center gap-0 lg:gap-4 text-center"
+            aria-label="Footer"
+          >
+            {navigation.main.map((item, i) => (
+              <Link
+                to={item.scroll}
+                href={"#"+item.scroll}
+                smooth={true}
+                duration={500}
+                offset={navigation.main.length - 1 === i ? -100 : -50}
+                key={i}
+                className="block text-md cursor-pointer font-display leading-6 hover:text-whiteColor duration-300"
+              >
+                    {item.name}
+              </Link>
+            ))}
+          </nav>
+          <div className=" text-whiteColor/60 my-8 lg:my-0 flex flex-col lg:flex-row justify-center gap-2 items-center lg:items-start lg:gap-8">
+            {navigation.social.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="font-display text-md hover:text-whiteColor duration-300 cursor-pointer"
+              >
+                <p className="" aria-hidden="true">
                   {item.name}
                 </p>
-              </div>
-            </Link>
-          ))}
-        </nav>
-        <div className="mt-5 text-whiteColor/60 flex justify-center gap-8">
-          {navigation.social.map((item) => (
-            <a
-              key={item.name}
-              href={item.href}
-              className="font-display text-md hover:text-whiteColor duration-300 cursor-pointer"
-            >
-              <p className="" aria-hidden="true">
-                {item.name}
-              </p>
-            </a>
-          ))}
+              </a>
+            ))}
+          </div>
         </div>
-        <p className="mt-5 text-whiteColor/60 font-display text-center text-sm leading-5 text-white">
+        <p className="mt-5 text-whiteColor/60 font-display text-center text-sm leading-5 pb-8 lg:pb-0 text-white">
           &copy; 2023 Katys Plumbing Service. All rights reserved.
         </p>
       </div>
